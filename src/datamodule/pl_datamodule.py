be9c45d63459b5752551a158fdf6aa1d1cefb601
XavR <xavroynard@hotmail.com>
2021-12-04 19:01:49 +0100

    reorganize folders + create files for lightning module and datamodule

---
+++ src/datamodule/pl_datamodule.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: Xavier Roynard
+"""
+
+#%% Imports
+import os
+import sys
+import glob
+import time
+
+import numpy as np
+
+#%% Functions
+
+#%% Classes
+
+#%% Tests
+
+#%% Main Script
+if __name__ == '__main__':
+    pass
